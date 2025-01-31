37412f52eb9006bec49708971c65e669b4e0c554
Gatasso <galassoworkstation@gmail.com>
2025-01-31 20:21:47 -0300

    feat: Model class w/ basic crud methods, serving as a parent to other entities

---
+++ src/Model/model.php
@@ -0,0 +1,120 @@
+<?php 
+abstract class Model
+{
+    private $attributes = [];
+    protected static $table;
+
+    public function __set($name, $value)
+    {
+        $this->attributes[$name] = $value;
+        return $this;
+    }
+    public function __get($name)
+    {
+        if (isset($this->attributes[$name])) {
+            return $this->attributes[$name];
+        } else{
+            return false;
+        }
+    }
+    public function __isset($name)
+    {
+        return isset($this->attributes[$name]);
+    }
+
+    protected function prepareData($data)
+    {
+        $res = array();
+        foreach ($data as $key => $value) {
+            if (is_scalar($value)) {
+                $res[$key] = $this->cleanData($value);
+            }
+        }
+    }
+    protected function cleanData($value)
+    {
+        if(is_string($value) & !empty($value)){
+            return "'".addslashes($value)."'";
+        } elseif (is_bool($value)){
+            return $value ? 'TRUE' : 'FALSE';
+        } elseif ($value !== ''){
+            return $value;
+        } else {
+            return 'NULL';
+        }
+    }
+
+    public function save()
+    {
+        $data = $this->prepareData($this->attributes);
+        if (!isset($this->id)) {
+            $query = "INSERT INTO " . $this->table . 
+            " (" . implode(', ', array_keys($this->data)) . ") VALUES " . 
+            "('" . implode("', '", array_values($this-> data)) . "')";
+        } else {
+            foreach ($this->data as $key => $value) {
+                if ($key !== 'id') {
+                    $auxArray[] = "{$key}={$value}";
+                }
+            }
+            $query = "UPDATE ". $this->table . 
+            " SET " . implode(',', $auxArray) .
+            " WHERE id='{$this->id}'";
+        }
+
+        if ($conn = Config::connect()) {
+            $stmt = $conn->prepare($query);
+            if ($stmt->execute()) {
+                return $stmt->rowCount();
+            } else{
+                return false;
+            }
+        }
+    }
+
+    public function findAll()
+    {
+        if ($conn = Config::connect()) {
+            $result = array();
+            $stmt = $conn->prepare('SELECT * from ' . $this->table);
+            if($stmt->execute()){
+                while ($res = $stmt->fetchObject(Model::class)) {
+                    $result[] = $res;
+                }
+            }  
+            if (count($result) > 0) {
+                return $result;
+            }
+        }
+        return false;
+    }
+
+    public function findById($id)
+    {
+        if($conn = Config::connect()){
+            $stmt = $conn->prepare("SELECT * from {$this->table} WHERE id={$id})");
+            if($stmt->execute()){
+                if ($stmt->rowCount() > 0) {
+                    $res = $stmt->fetchObject('Model');
+                    if ($res) {
+                        return $res;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public function deleteById($id)
+    {
+        if($conn = Config::connect()){
+            $stmt = $conn->prepare("DELETE FROM {$this->table} WHERE id={$id}");
+            if($stmt->execute()){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
+?>
