813a8d8e6d9765f964ab210eff7dcd3997c8aff1
Gatasso <galassoworkstation@gmail.com>
2025-02-06 21:17:21 -0300

    feat: update the view layer, creating the basic frontend possible

--- src/View/grade.php
+++ src/View/grade.php
@@ -1,30 +1,40 @@
-<h1>Lista de Livros</h1>
+<div>
+    <h1>Lista de Livros</h1>
     <table border="1">
         <thead>
             <tr>
                 <th>ID</th>
                 <th>Título</th>
                 <th>Autor</th>
+                <th>Editora</th>
+                <th>Páginas</th>
+                <th>Categoria</th>
+                <th>Lido</th>
                 <th>Ações</th>
             </tr>
         </thead>
         <tbody>
             <?php
-                require_once __DIR__ . '/../Controller/bookController.php';
-                $controller = new BookController();
-                $books = $controller->list();
                 
-                foreach ($books['books'] as $book) {
+                echo "Número de livros: " . count($books);
+                
+                foreach ($books as $book) {  // Itera nos livros retornados pelo controlador
                     echo "<tr>
-                            <td>{$book['id']}</td>
-                            <td>{$book['title']}</td>
-                            <td>{$book['author']}</td>
-                            <td>
-                                <a href='../Controller/bookController.php?action=edit&id={$book['id']}'>Editar</a> |
-                                <a href='../Controller/bookController.php?action=delete&id={$book['id']}'>Excluir</a>
-                            </td>
-                          </tr>";
+                    <td>{$book->id}</td>
+                    <td>{$book->title}</td>
+                    <td>{$book->author}</td>
+                    <td>{$book->publisher}</td>
+                    <td>{$book->num_pages}</td>
+                    <td>{$book->category}</td>
+                    <td>{$book->is_read}</td>
+
+                    <td>
+                    <a href='./index.php?controller=BookController&method=edit&id={$book->id}'>Editar</a> |
+                    <a href='./index.php?controller=BookController&method=delete&id={$book->id}'>Excluir</a>
+                    </td>
+                    </tr>";
                 }
             ?>
         </tbody>
     </table>
+</div>
--- src/View/index.php
+++ src/View/index.php
@@ -1,23 +1,23 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors', true);
+ini_set('display_errors', 1);
 
 spl_autoload_register(function($class) {
     $directories = [
-        'Controller',
-        'Model',
-        'View',
-        'Config'
+        '../Controller',  
+        '../Model',
+        '../Config',
     ];
 
     foreach ($directories as $dir) {
-        $file = $dir . $class . '.php';
+        $file = $dir . DIRECTORY_SEPARATOR . $class . '.php';
         
         if (file_exists($file)) {
             require_once $file;
             return true;
         }
     }
+    
     return false;
 });
 ?>
@@ -26,24 +26,28 @@ spl_autoload_register(function($class) {
     <header>
         <meta charset="utf-8">
         <title>Gerenciamento de Livros</title>
-        <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css&#39; rel='stylesheet' integrity='sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB' crossorigin='anonymous' />
-        <link href='https://use.fontawesome.com/releases/v5.1.0/css/all.css&#39; rel='stylesheet' integrity='sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt' crossorigin='anonymous' />
-        <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js&#39; integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js&#39; integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossorigin='anonymous'></script>
-        <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js&#39; integrity='sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T' crossorigin='anonymous'></script>
     </header>
     <body>
 
-        <?php
+    <?php
         if ($_GET) {
-            $controller = isset($_GET['controller']) ? ((class_exists($_GET['controller'])) ? new $_GET['controller'] : NULL ) : null;
-            $method     = isset($_GET['method']) ? $_GET['method'] : null;
-            if ($controller && $method) {
-                if (method_exists($controller, $method)) {
+            $controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : null;
+            $method = isset($_GET['method']) ? $_GET['method'] : null;
+
+            $controllerClass = "$controllerName";
+
+            if ($controllerName && class_exists($controllerClass)) {
+                $controller = new $controllerClass;
+                
+                if ($method && method_exists($controller, $method)) {
                     $parameters = $_GET;
                     unset($parameters['controller']);
                     unset($parameters['method']);
-                    call_user_func(array($controller, $method), $parameters);
+                    if (isset($parameters['id'])) {
+                        call_user_func_array([$controller, $method], [$parameters['id']]);
+                    } else{
+                        call_user_func([$controller,$method], $parameters);
+                    }
                 } else {
                     echo "Método não encontrado!";
                 }
@@ -52,10 +56,11 @@ spl_autoload_register(function($class) {
             }
         } else {
             echo '<nav><ul><li><a href="./index.php">Início</a></li>';
-            echo '<li><a href="../Controller/bookController.php?action=create">Adicionar Livro</a></li>';
-            echo '<li><a href="../Controller/bookController.php?action=list">Listar Livros</a></li></ul></nav>'; 
+            echo '<li><a href="./index.php?controller=BookController&method=create">Adicionar Livro</a></li>';
+            echo '<li><a href="./index.php?controller=BookController&method=list">Listar Livros</a></li></ul></nav>'; 
         }
-        ?>
+    ?>
+
 
     </body>
 </html>
